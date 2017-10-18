539a214995557cb627825362fb63af0ef07840de
speedymh <matthias_hoppe@outlook.de>
2017-10-18 23:08:39 +0200

    Primary Cards hinzugefügt; Team Seite erstellt

--- function.js
+++ function.js
@@ -30,11 +30,111 @@ $(document).ready(function(){
     $('li.grace, li.about, ul.about-us, ul.grace-schools, span.schools, span.about').removeClass('clicked');
   });
 
+// Rules
+
+  $('.pop-up.rules').on('click', function() {
+    $('.card.rules').toggleClass('open');
+  });
+  $('.cross').on('click', function() {
+    $('.card.rules').removeClass('open');
+  });
+
+// Fees
+
+  $('.pop-up.fees').on('click', function() {
+    $('.card.fees').toggleClass('open');
+  });
+  $('.cross').on('click', function() {
+    $('.card.fees').removeClass('open');
+  });
+
+// Nursery Teachers
+
+  $('.pop-up.teachers-nursery').on('click', function() {
+    $('.card.nursery').toggleClass('open');
+  });
+  $('.cross').on('click', function() {
+    $('.card.nursery').removeClass('open');
+  });
+
+// Nursery Non-Teaching
+
+  $('.pop-up.non-t-nursery').on('click', function() {
+    $('.card.non-t-nursery').toggleClass('open');
+  });
+  $('.cross').on('click', function() {
+    $('.card.non-t-nursery').removeClass('open');
+  });
+
+// Primary Teachers
+
+  $('.pop-up.teachers-primary').on('click', function() {
+    $('.card.primary-card').toggleClass('open');
+  });
+  $('.cross').on('click', function() {
+    $('.card.primary-card').removeClass('open');
+  });
+
+// Primary Non-Teaching
+
+  $('.pop-up.non-t-primary').on('click', function() {
+    $('.card.non-t-primary').toggleClass('open');
+  });
+  $('.cross').on('click', function() {
+    $('.card.non-t-primary').removeClass('open');
+  });
+
+// Secondary Teachers
+
+  $('.pop-up.teachers-secondary').on('click', function() {
+    $('.card.secondary-card').toggleClass('open');
+  });
+  $('.cross').on('click', function() {
+    $('.card.secondary-card').removeClass('open');
+  });
+
+// Secondary Non-Teaching
+
+  $('.pop-up.non-t-secondary').on('click', function() {
+    $('.card.non-t-secondary').toggleClass('open');
+  });
+  $('.cross').on('click', function() {
+    $('.card.non-t-secondary').removeClass('open');
+  });
+
+    /* Hide all elements outside the visible window */
+    //
+    // var top_of_object = $(this).position().top;
+    // var bottom_of_window = $(window).scrollTop() + $(window).height();
+
+    // $('.hero-image').css('transform': 'translateY(0px)');
+
+    /* Every time the window is scrolled ... */
+//     $(window).scroll( function(){
+//
+//     /* Check the location of the desired elements */
+//     $('.hide').each( function(i){
+//
+//         var bottom_of_object = $(this).position().top + $(this).outerHeight() / 2;/*here is the change*/
+//         var bottom_of_window = $(window).scrollTop() + $(window).height();
+//
+//
+//             if( bottom_of_window > ( bottom_of_object + 20 )  ){
+//
+//                 $(this).removeClass('hide').animate({'opacity':'1'},500);
+//
+//             }
+//
+//     });
+//
+// });
+
+});
+
   // var menuWidth = $('.primary').css(width);
   // $('.secondary').css('width', 'menuWidth');
 
   // smoothScroll(1000);
-});
 
 // function smoothScroll (duration) {
 //   $('.chevron-down').on('click', function(event) {
